a88605e8cb888f8fdfbfce0e19c817023966c04a
Fouziya <fouzifathii@gmail.com>
2022-08-28 23:12:39 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,15 +8,23 @@ let confirmPassword = document.getElementById("CPassword");
 let img = document.getElementById("avatar");
 let send = document.getElementById("send");
 let error = document.getElementById("Error");
+//err
+let fNameErr = document.getElementById("fnameError");
+let lNameErr = document.getElementById("lnameError");
+let genderErr = document.getElementById("genderError");
+let dobErr = document.getElementById("bdayError");
+let mailErr = document.getElementById("emailError");
+let pwErr = document.getElementById("pwordError");
+let confirmErr = document.getElementById("confirmError");
 
 send.addEventListener("click", (e) => {
   //FirstName
   if (fName.value === "" || fName.value === null || fName.value.match(/\d/gi)) {
-    alert("Please Fill Valid First Name");
+    fNameErr.innerHTML = "Please Fill Valid First Name";
   }
   //LastName
   if (lName.value === "" || lName.value === null || lName.value.match(/\d/gi)) {
-    alert("Please Fill Valid Last Name");
+    lNameErr.innerHTML = "Please Fill Valid Last Name";
   }
   //Gender
   let valid = false;
@@ -25,11 +33,11 @@ send.addEventListener("click", (e) => {
     break;
   }
   if (valid == false) {
-    alert("please select valid gender");
+    genderErr.innerHTML = "please select valid gender";
   }
   //Dob
   if (dob.value == "" || dob.value == null) {
-    alert("DOB is required");
+    dobErr.innerHTML = "DOB is required";
   } else {
     let d = new Date();
     let a = new Date(dob.value);
@@ -39,32 +47,32 @@ send.addEventListener("click", (e) => {
     var age = Math.abs(newYear - 1970);
 
     if (age < 18) {
-      alert("You Are Not Eligible");
+      dobErr.innerHTML = "You Are Not Eligible";
     }
   }
   //email
   let pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
   if (!mail.value.match(pattern)) {
-    alert("Enter Valid Email");
+    mailErr.innerHTML = "Enter Valid Email";
   }
 
   //passWord and confirm password
   if (password.value.length < 8) {
-    alert("Password Length Must Have Atlease 8 Characters");
+    pwErr.innerHTML = "Password Length Must Have Atlease 8 Characters";
   } else if (password.value.search(/[0-9]/g) === -1) {
-    alert("Password  Must Have Atlease 1 Number");
+    pwErr.innerHTML = "Password  Must Have Atlease 1 Number";
   } else if (password.value.search(/[a-z]/g) === -1) {
-    alert("Password  Must Have Atlease 1 Lower Case Letter");
+    pwErr.innerHTML = "Password  Must Have Atlease 1 Lower Case Letter";
   } else if (password.value.search(/[A-Z]/g) === -1) {
-    alert("Password  Must Have Atlease 1 Upper Case Letter");
+    pwErr.innerHTML = "Password  Must Have Atlease 1 upper Case Letter";
   } else if (
     password.value.search(/[!\@\#\$\%\^\&\*\(\)\_\-\+\=\.\,\;\:\'\"]/g) === -1
   ) {
-    alert("Password  Must Have Atlease 1 Special Character");
+    pwErr.innerHTML = "Password  Must Have Atlease 1 Special Character";
   }
 
   if (password.value !== confirmPassword.value) {
-    alert("Password And Confirm Password are Not The Same");
+    confirmErr.innerHTML = "Password And Confirm Password are Not The Same";
   }
   e.preventDefault();
 });
